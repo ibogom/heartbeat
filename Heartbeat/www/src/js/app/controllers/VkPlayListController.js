0e4f9e87bd02e1101a25aa8db2baff2255ec84da
Ievgen Bogomolov <ievgen.bogomolov@gmail.com>
2015-03-03 18:59:10 +0200

    Swipe down helper done

--- Heartbeat/www/src/js/app/controllers/VkPlayListController.js
+++ Heartbeat/www/src/js/app/controllers/VkPlayListController.js
@@ -18,46 +18,70 @@ define([
     return Backbone.Marionette.Controller.extend({
         initialize: function () {
             this.vkAudioCollection = new VkAudioCollection();
-            if(window.localStorage.getItem("songs") === null){
-               window.localStorage.setItem("songs","");
+            if (window.localStorage.getItem("songs") === null) {
+                window.localStorage.setItem("songs", "");
             }
             this.songs = window.localStorage.getItem("songs");
         },
         getAudio: function (songsNumber) {
             this.vkAudioCollection.songsNumber = songsNumber;
-            (this.songs.length > 0)? this.getLocalAudio() : this.getAudioRequest();
+            (this.songs.length > 0) ? this.getLocalAudio() : this.getAudioRequest();
             SwipeDownHelper.action(this);
         },
-        getLocalAudio: function(){
+        getLocalAudio: function () {
             this.vkAudioCollection.setData();
             this.showView();
         },
-        getAudioRequest: function(){
+        getAudioRequest: function () {
             var self = this,
                 date = new Date();
             window.loader.show();
             this.vkAudioCollection.fetch().done(function (result) {
                 var songsCount = result.response.shift();
                 $(App.layout.refresh.el).find(".update-date").empty().text(date.toLocaleString());
-                self.saveData(result.response);
-                self.compareResponse(result.response);
-                self.showView();
+                self.difItems = self.compareResponse(result.response);
+                if(self.difItems.length >= 0 && this.newItemAdded === false){
+
+                }else{
+                    self.saveData(result.response);
+                    self.showView();
+                }
             });
         },
-        showView: function(){
+        showView: function () {
             this.playListCollectionView = new PlayListCollectionView({
                 collection: this.vkAudioCollection
             });
-            if(!$(App.playlistLayout.list.el).html().trim()){
+            if (!$(App.playlistLayout.list.el).html().trim()) {
                 App.playlistLayout.list.show(this.playListCollectionView);
             }
             window.loader.hide();
         },
-        refresh: function(){
+        refresh: function () {
             this.getAudioRequest();
         },
-        compareResponse: function(response){
-            var localData = this.vkAudioCollection;
+        compareResponse: function (items) {
+            var diff, i = 0;
+            if (window.localStorage.getItem("songs") !== null) {
+                var localData = JSON.parse(window.localStorage.getItem("songs")),
+                    detectArrayLength = (items.length >= localData.length) ? items : localData;
+                this.newItemAdded = (items.length !== localData.length) ? true : false;
+                diff = _.without(_.uniq(_.map(detectArrayLength, function (itemsObg) {
+                    var currObj = localData[i++],changedItem;
+                    if(itemsObg.artist !== currObj.artist || itemsObg.title !== currObj.title ||
+                        itemsObg.url !== currObj.url){
+                        changedItem = itemsObg;
+                    } else{
+                        changedItem = false;
+                    }
+                    return changedItem;
+                })), false);
+            } else {
+                diff = [];
+                this.newItemAdded = true;
+            }
+            return diff;
+
         },
         saveData: function (result) {
             var id = 0;
@@ -76,10 +100,10 @@ define([
                     title: songs.title,
                     url: songs.url,
                     id: id++,
-                    isPlayed:false
+                    isPlayed: false
                 };
             });
-            window.localStorage.setItem("songs",JSON.stringify(songs));
+            window.localStorage.setItem("songs", JSON.stringify(songs));
             this.vkAudioCollection.reset(songs);
         }
     });
